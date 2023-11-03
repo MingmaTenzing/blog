4119f0936cdecdebc1bb1161e22dd65b12f22089
MingmaTenzing <pasangsherpa689@gmail.com>
2023-11-03 13:32:00 +0800

    reading pipe removed

--- src/app/post/reading-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReadingTimePipe } from './reading-time.pipe';
-
-describe('ReadingTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReadingTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
