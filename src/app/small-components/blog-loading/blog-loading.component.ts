852f4473450d6b935ce8bbf5c8c5f61dc196c985
MingmaTenzing <pasangsherpa689@gmail.com>
2023-11-09 14:16:03 +0800

    need to work on the blog loading for now you can merge the category

---
+++ src/app/small-components/blog-loading/blog-loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-loading',
+  templateUrl: './blog-loading.component.html',
+  styleUrls: ['./blog-loading.component.css']
+})
+export class BlogLoadingComponent {
+
+}
---
+++ src/app/small-components/blog-loading/blog-loading.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogLoadingComponent } from './blog-loading.component';
+
+
+
+@NgModule({
+  declarations: [
+    BlogLoadingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogLoadingModule { }
